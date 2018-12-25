651ce0705afc033f9ee5e09fb8d9a8ad001e24e8
amirul ikmal <amirulikmal.biz@gmail.com>
2018-12-25 16:05:08 +0800

    init input and button on key press

--- app.js
+++ app.js
@@ -10,7 +10,26 @@ var budgetController = (function() {
 // UI Controller 
 var UIController = (function() {
     
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
     
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
     
 })();
 
@@ -20,6 +39,40 @@ var UIController = (function() {
 // Global Application Controller 
 var controller = (function(budgetCtrl, UICtrl) {
     
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+        
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
     
+    var ctrlAddItem = function() {
+        
+        // 1. Get the field input data
+        var input = UICtrl.getInput();
+        // 2. Add the item to the budget controller 
+        
+        // 3. Add the item to the UI 
+        
+        // 4. Calculate the budget
+        
+        // 5. Display the budget on the UI
+        
+    };
     
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();    
+        }
+    };
+
 })(budgetController, UIController);
+
+controller.init();
+
