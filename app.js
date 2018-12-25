a2238b310793d41bc2c2d96ef77fe40898d0ba2c
amirul ikmal <amirulikmal.biz@gmail.com>
2018-12-25 14:15:07 +0800

    Setting up the main controller for the application

---
+++ app.js
@@ -0,0 +1,25 @@
+// Budget Controller 
+var budgetController = (function() {
+    
+    
+    
+})();
+
+
+
+// UI Controller 
+var UIController = (function() {
+    
+    
+    
+})();
+
+
+
+
+// Global Application Controller 
+var controller = (function(budgetCtrl, UICtrl) {
+    
+    
+    
+})(budgetController, UIController);
